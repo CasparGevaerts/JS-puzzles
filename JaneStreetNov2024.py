5b6ec04eb57d247507207ecb345c205080a1cdbd
Caspar <caspargevaerts@gmail.com>
2024-12-19 16:11:40 +0100

    Update JaneStreetNov2024.py
    Added comments to the Nov puzzle

--- JaneStreetNov2024.py
+++ JaneStreetNov2024.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from scipy.integrate import dblquad
 
+# Define the probability of the point existing. This is two semi-cirkles that go through point A, with the corners of the square being their centre, excluding their overlap.
 def compute_probability(x, y):
     R = np.sqrt(x ** 2 + y ** 2)
     r = np.sqrt(x ** 2 + (1 - y) ** 2)
@@ -14,6 +15,8 @@ def compute_probability(x, y):
     area_overlap = part1 + part2 - part3
     return 1/4 * area_c1 + 1/4 * area_c2 - area_overlap
 
+# Compute the integral over 1/8th of the total area (the other parts have the same value due to symmetries.
 area = dblquad(compute_probability,0, 0.5, 0, lambda x: x)
+# Multiply by 8 to get the total value.
 answer = area[0]*8
 print(answer)
